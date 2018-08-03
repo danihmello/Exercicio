9faf9fb604ffe0330f5946a0ba0476df6a87bae1
IsacSouzaCampos <42068215+IsacSouzaCampos@users.noreply.github.com>
2018-08-03 11:01:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,32 @@
-public class Main{
+public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Visao v = new Visao();
         Operacao operacao = new Operacao();
-        Scanner input = new Scanner(System.in);
         
         operacao.soma();
         
-        double n1,n2;
-        System.out.println("Digite o primeiro numero inteiro");
-        n1 = input.nextInt();  
-        System.out.println("Digite o segundo numero inteiro");
-        n2 = input.nextInt(); 
+        int n1 = v.inInt("Digite o primeiro numero inteiro");
+        int n2 = v.inInt("Digite o segundo numero inteiro");
         operacao.calculaMedia(n1,n2);
         
-        int resposta;
-        resposta = operacao.multiplicacao();
-        System.out.println("O produto dos numeros e " + resposta);
+        int resposta = operacao.multiplicacao();
+        v.inString("O produto dos numeros e " + resposta);
         
         double num1,num2, resposta2;
-        System.out.println("Digite o dividendo");
-        num1 = input.nextInt();  
-        System.out.println("Digite o divisor");
-        num2 = input.nextInt(); 
+        num1 = v.inDouble("Digite o dividendo");
+        num2 = v.inDouble("Digite o divisor");
         resposta2 = operacao.divisao(num1, num2);
-        System.out.println("O resultado da divisao dos numeros e " + resposta2);
+        v.inString("O resultado da divisao dos numeros e " + resposta2);
+        
+        operacao.subtracao();
+        
+        num1 = v.inDouble("Numero 1: ");
+        double peso1 = v.inDouble("Peso do numero 1: ");
+        num2 = v.inDouble("Numero 2: ");
+        double peso2 = v.inDouble("Peso do numero 2: ");
+        v.inString("Media: "+operacao.mediaPonderada(num1, num2, peso1, peso2));
+        
+        operacao.calculaFatorial(v.inInt("Digite um valor inteiro: "));
     }
-    //edição
 }
