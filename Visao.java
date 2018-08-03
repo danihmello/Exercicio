046ccc84ecee2ce674b15af2e9994631969a1d61
IsacSouzaCampos <42068215+IsacSouzaCampos@users.noreply.github.com>
2018-08-03 10:49:22 -0300

    Add files via upload

---
+++ Visao.java
@@ -0,0 +1,25 @@
+
+package exercicio_operacoes;
+
+import java.util.Scanner;
+
+public class Visao {
+    Scanner in = new Scanner(System.in);
+    
+    public int inInt(String s){
+        System.out.print(s);
+        int v = in.nextInt();
+        in.nextLine();
+        
+        return v;
+    }
+    
+    public double inDouble(String s){
+        System.out.print(s);
+        return in.nextDouble();
+    }
+    
+    public void inString(String s){
+        System.out.print(s);
+    }
+}
